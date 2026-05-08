9085785a7c8e7f50a47ee0ccd2f3ff43d726d5da
ShivangChaurasia <shiva17ng@gmail.com>
2026-05-08 10:16:44 +0530

    fix: update success message in root route

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("CI/CD Pipeline Working Successfully 🚀,.. it workedd!!!");
+  res.send("CI/CD Pipeline Working Successfully........!!!");
 });
 
 app.get("/health", (req, res) => {
