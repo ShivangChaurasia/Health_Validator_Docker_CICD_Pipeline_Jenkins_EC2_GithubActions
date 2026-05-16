1bbe266fa5603738699aa06a05b7595496252619
ShivangChaurasia <shiva17ng@gmail.com>
2026-05-16 16:44:55 +0530

    fix: update root route message to include project name

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("CI/CD Pipeline Working Successfully........!!!");
+  res.send("CI/CD Pipeline Working Successfully........!!! This is health Validator");
 });
 
 app.get("/health", (req, res) => {
